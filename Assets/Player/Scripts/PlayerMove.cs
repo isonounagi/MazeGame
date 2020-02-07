6e44d335787b4075353f014d057ef2c25ffd79dd
isonounagi <isonounagi@gmail.com>
2020-02-07 23:58:34 +0900

    プレイヤーのアニメーションアセット導入

---
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 velocity;
+    [SerializeField]
+    private float walkSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        characterController.Move(velocity * walkSpeed * Time.deltaTime);
+    }
+}
