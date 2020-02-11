f5b4bab7b2395ed2567a43dacca34eb9902ab7e4
isonounagi <isonounagi@gmail.com>
2020-02-11 23:10:14 +0900

    カメラ操作　3Dと2Dの切り換え　それぞれのときの操作の違い　3D時のカメラ位置、カメラの回転

--- Assets/Player/Scripts/PlayerMove.cs
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
-{
+{   
     private CharacterController characterController;
-    private Vector3 velocity;
-    [SerializeField]
-    private float walkSpeed;
     private Animator animator;
+    private Vector3 velocity;
+    
+    [SerializeField]
+    private float moveSpeed;
 
     // Use this for initialization
     void Start()
@@ -20,21 +21,59 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
+        if (CameraChange.mainCameraActivate % 2 == 0)
         {
-            velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            if (characterController.isGrounded)
+            {
+                velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
-            if (velocity.magnitude > 0.1f)
-            {
-                animator.SetFloat("Speed", velocity.magnitude);
-                transform.LookAt(transform.position + velocity);
-            }
-            else
-            {
-                animator.SetFloat("Speed", 0f);
+                if (velocity.magnitude > 0.1f)
+                {
+                    animator.SetFloat("Speed", velocity.magnitude);
+                    transform.LookAt(transform.position + velocity);
+                }
+                else
+                {
+                    animator.SetFloat("Speed", 0f);
+                }
+
             }
         }
+        else
+        {
+            velocity = new Vector3(0, 0, 0);
+
+            if (characterController.isGrounded)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    characterController.Move(this.gameObject.transform.forward * moveSpeed * Time.deltaTime);//前方にMoveSpeed * 時間経過分動かす
+                }
+                /*if (Input.GetKey(KeyCode.S))
+                {
+                    characterController.Move(this.gameObject.transform.forward * -1 * moveSpeed * Time.deltaTime);//後方にMoveSpeed * 時間経過分動かす
+                }*/
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Rotate(new Vector3(0, -5, 0));//左回転する        
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Rotate(new Vector3(0, 5, 0));//右回転する        
+                }
+
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    animator.SetFloat("Speed", 1f);
+                }
+                else
+                {
+                    animator.SetFloat("Speed", 0f);
+                }
+            }
+        }
+        
         velocity.y += Physics.gravity.y * Time.deltaTime;
-        characterController.Move(velocity * walkSpeed * Time.deltaTime);
+        characterController.Move(velocity * moveSpeed * Time.deltaTime);
     }
 }
