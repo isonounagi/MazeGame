642d5b670883610d018c3e4770f6d12875d7507f
isonounagi <isonounagi@gmail.com>
2020-02-29 23:28:19 +0900

    新しいアイテムのアセットを追加

---
+++ Assets/Item/Scripts/ItemGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    private GameObject[] AutoWallPosTag;
+    private int getLength;
+    
+    [SerializeField]
+    public GameObject hummer;
+
+    private void SetItem(GameObject itemPrefab)
+    {
+            for (int p = 1; p <= getLength - 1; p++)
+            {
+                if (AutoWallPosTag[p].tag == "Path")
+                {
+                    int r = Random.Range(1, 11);
+                    if (r == 1)
+                    {
+                        Instantiate(itemPrefab,AutoWallPosTag[p].transform);
+                    }
+
+                }
+            }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AutoWallPosTag = GameObject.FindGameObjectsWithTag("Path");
+
+        getLength = AutoWallPosTag.Length;
+
+        Debug.Log(getLength);
+
+        SetItem(hummer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
