b5095c4384760821be310abde4dcdc85160ff126
isonounagi <isonounagi@gmail.com>
2020-03-05 14:32:35 +0900

    敵接触時にテキスト表示後、自動でリスタート

--- Assets/UI/Scripts/UIText.cs
+++ Assets/UI/Scripts/UIText.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIText : MonoBehaviour
 {
@@ -13,6 +14,23 @@ public class UIText : MonoBehaviour
         }
     }
 
+    private void SetText() //テキストのUIを表示する
+    {
+        UIDirector.goal.enabled = true;
+    }
+
+    private void SetEnd()
+    {
+        UIDirector.goal.text = "GAME OVER";
+        UIDirector.goal.enabled = true;
+    }
+
+    private void ReStart()
+    {
+        CameraChange.mainCameraActivate = 1;
+
+        SceneManager.LoadScene(0);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +41,10 @@ public class UIText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(CameraChange.mainCameraActivate == 0)　//敵につかまったとき、ゲームオーバーの文字を表示後、リスタート
+        {
+            Invoke("SetEnd", 3.0f);
+            Invoke("ReStart", 4.5f);
+        }
     }
 }
